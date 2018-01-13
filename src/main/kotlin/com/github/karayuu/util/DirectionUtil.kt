186dbb68026ca033ba8874520f379290c36851b8
karayuumac <karaatena0706@gmail.com>
2018-01-13 20:55:37 +0900

    不要ファイルの削除・リファクタリング

--- src/main/kotlin/com/github/karayuu/util/DirectionUtil.kt
+++ src/main/kotlin/com/github/karayuu/util/DirectionUtil.kt
@@ -18,11 +18,10 @@ fun Player.getDirection() : Direction {
 
     if (rotation < 0) rotation += 360
 
-    if (0 <= rotation && rotation < 45) return Direction.NORTH
+    if (0 <= rotation && rotation < 45 || 315 <= rotation && rotation < 360) return Direction.NORTH
     if (45 <= rotation && rotation < 135) return Direction.EAST
     if (135 <= rotation && rotation < 225) return Direction.SOUTH
     if (225 <= rotation && rotation < 315) return Direction.WEST
-    if (315 <= rotation && rotation < 360) return Direction.NORTH
     throw IllegalStateException("[CreateGridRegionPlugin]方角処理で重大なエラー。開発者に報告してください。")
 }
 
@@ -37,7 +36,7 @@ fun Player.getDirectionString(type: GridRegion.DirectionType) : String {
 
     if (rotation < 0) rotation += 360
 
-    if (0 <= rotation && rotation < 45) {
+    if (0 <= rotation && rotation < 45 || 315 <= rotation && rotation < 360) {
         result[GridRegion.DirectionType.BEHIND] = "南(South)"
         result[GridRegion.DirectionType.AHEAD] = "北(North)"
         result[GridRegion.DirectionType.LEFT] = "西(West)"
@@ -57,11 +56,6 @@ fun Player.getDirectionString(type: GridRegion.DirectionType) : String {
         result[GridRegion.DirectionType.RIGHT] = "北(North)"
         result[GridRegion.DirectionType.AHEAD] = "西(West)"
         result[GridRegion.DirectionType.BEHIND] = "東(East)"
-    } else if (315 <= rotation && rotation < 360) {
-        result[GridRegion.DirectionType.BEHIND] = "南(South)"
-        result[GridRegion.DirectionType.AHEAD] = "北(North)"
-        result[GridRegion.DirectionType.LEFT] = "西(West)"
-        result[GridRegion.DirectionType.RIGHT] = "東(East)"
     }
     return result[type] ?: throw IllegalStateException("[CreateGridRegionPlugin]方角処理で重大なエラー。開発者に報告してください。")
 }
