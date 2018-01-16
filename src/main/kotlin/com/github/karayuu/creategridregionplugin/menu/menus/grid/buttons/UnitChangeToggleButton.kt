b9dcbe9f90ae156d3e2bed3fd05ebbd85a097d88
Kory33 <6561358+kory33@users.noreply.github.com>
2018-01-16 21:14:25 +0800

    UPDATE: UnitChangeToggleButtonクラスを追加する

---
+++ src/main/kotlin/com/github/karayuu/creategridregionplugin/menu/menus/grid/buttons/UnitChangeToggleButton.kt
@@ -0,0 +1,36 @@
+package com.github.karayuu.creategridregionplugin.menu.menus.grid.buttons
+
+import com.github.karayuu.creategridregionplugin.menu.component.Button
+import com.github.karayuu.creategridregionplugin.menu.component.Icon
+import com.github.karayuu.creategridregionplugin.menu.menus.grid.GridMenuIssuerR
+import com.github.karayuu.creategridregionplugin.player.property.GridSelection
+import com.github.karayuu.creategridregionplugin.util.openInventoryOf
+import org.bukkit.ChatColor
+import org.bukkit.Material
+import org.bukkit.event.inventory.InventoryClickEvent
+
+/**
+ * ユニット数の増減量をトグルするボタンを表すクラスです。
+ */
+class UnitChangeToggleButton(private val gridSelection: GridSelection,
+                             private val menuIssuer: GridMenuIssuerR): Button {
+    override val icon = getIcon(gridSelection)
+
+    override val action: (InventoryClickEvent) -> Unit = { event ->
+        val newGridSelection = gridSelection.toggleUnitChange()
+        event.whoClicked.openInventoryOf(menuIssuer.replicateWith(newGridSelection))
+    }
+
+    private fun getIcon(gridSelection: GridSelection) = Icon(
+            Material.STAINED_GLASS_PANE,
+            name = "${ChatColor.GREEN}拡張単位の変更",
+            lore = listOf(
+                    "${ChatColor.GREEN}現在のユニット指定量",
+                    "${ChatColor.AQUA}${gridSelection.unitChange}${ChatColor.GREEN}ユニット" +
+                            "(${ChatColor.AQUA}${gridSelection.unitChange.toBlockWidth()}${ChatColor.GREEN}ブロック)" +
+                            "/1クリック",
+                    "${ChatColor.RED}${ChatColor.UNDERLINE}クリックで変更"
+            )
+    )
+
+}
