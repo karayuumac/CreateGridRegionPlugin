b6c1a853a13933c776d683881016ed2736208911
Kory33 <6561358+kory33@users.noreply.github.com>
2018-01-16 21:37:02 +0800

    UPDATE: RelativeDirectionに方向の日本語訳のメンバを追加

--- src/main/kotlin/com/github/karayuu/creategridregionplugin/util/RelativeDirection.kt
+++ src/main/kotlin/com/github/karayuu/creategridregionplugin/util/RelativeDirection.kt
@@ -3,11 +3,11 @@ package com.github.karayuu.creategridregionplugin.util
 /**
  * 相対的な方向を示す列挙型です。
  */
-enum class RelativeDirection {
-    AHEAD,
-    RIGHT,
-    BEHIND,
-    LEFT;
+enum class RelativeDirection(val localizedName: String) {
+    AHEAD("前"),
+    RIGHT("右"),
+    BEHIND("後ろ"),
+    LEFT("左");
 
     /**
      * このオブジェクトが指し示す方向を向いた時に右手にある方向を取得します。
