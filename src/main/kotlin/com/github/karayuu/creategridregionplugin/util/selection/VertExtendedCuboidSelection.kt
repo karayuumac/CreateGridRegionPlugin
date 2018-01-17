616686dc34515829912c919a0d550abbc18536a2
Kory33 <6561358+kory33@users.noreply.github.com>
2018-01-17 20:08:27 +0800

    CLEAN: クラス説明を追加する

--- src/main/kotlin/com/github/karayuu/creategridregionplugin/util/selection/VertExtendedCuboidSelection.kt
+++ src/main/kotlin/com/github/karayuu/creategridregionplugin/util/selection/VertExtendedCuboidSelection.kt
@@ -5,5 +5,10 @@ import com.sk89q.worldedit.Vector
 import com.sk89q.worldedit.bukkit.selections.CuboidSelection
 import org.bukkit.World
 
+/**
+ * 最低高度から最大高度までの高さを持つ直方体選択領域を表すクラス
+ *
+ * @author kory33
+ */
 class VertExtendedCuboidSelection(world: World, start: Vec2, end: Vec2)
     : CuboidSelection(world, Vector(start.first, 0.0, start.second), Vector(end.first, 256.0, end.second))
