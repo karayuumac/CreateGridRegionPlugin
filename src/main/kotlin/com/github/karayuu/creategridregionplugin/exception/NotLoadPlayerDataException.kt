81f95ed498d87005dd39b60aad98d5a774e3ed0c
Kory33 <6561358+kory33@users.noreply.github.com>
2018-01-17 20:06:59 +0800

    CLEAN: 未使用のクラスと拡張関数を削除する

--- src/main/kotlin/com/github/karayuu/creategridregionplugin/exception/NotLoadPlayerDataException.kt
+++
@@ -1,8 +0,0 @@
-package com.github.karayuu.creategridregionplugin.exception
-
-/**
- * Created by karayuu on 2017/12/25
- * Developer of Gigantic☆Seichi Server
- * Support at dev-basic or dev-extreme channel of Discord
- */
-class NotLoadPlayerDataException(override val message: String) : Exception()
--- src/main/kotlin/com/github/karayuu/creategridregionplugin/util/direction/DirectionUtil.kt
+++ src/main/kotlin/com/github/karayuu/creategridregionplugin/util/direction/DirectionUtil.kt
@@ -1,55 +1,12 @@
 package com.github.karayuu.creategridregionplugin.util.direction
 
 import org.bukkit.Location
-import org.bukkit.entity.Player
 
 /**
  * Created by karayuu on 2017/12/29
  * Developer of Gigantic☆Seichi Server
  * Support at dev-basic or dev-extreme channel of Discord
  */
-
-/**
- * プレイヤーが向いている方位を取得します
- * @return [CardinalDirection]で表される方位
- */
-fun Player.getCardinalDirection() = CardinalDirection.fromLocation(location)
-
-/**
- * プレイヤーの方角を日本語で取得します
- * @param type プレイヤーの前・後ろ・右・左(DirectionType)
- * @deprecated use [cardinalDirectionOn]
- * @return 方角[日本語(英語)]
- */
-fun Player.getDirectionString(type: RelativeDirection) : String {
-    var rotation: Float = (player.location.yaw + 180) % 360
-    val result: MutableMap<RelativeDirection, String> = mutableMapOf()
-
-    if (rotation < 0) rotation += 360
-
-    if (0 <= rotation && rotation < 45 || 315 <= rotation && rotation < 360) {
-        result[RelativeDirection.BEHIND] = "南(South)"
-        result[RelativeDirection.AHEAD] = "北(North)"
-        result[RelativeDirection.LEFT] = "西(West)"
-        result[RelativeDirection.RIGHT] = "東(East)"
-    } else if (45 <= rotation && rotation < 135) {
-        result[RelativeDirection.RIGHT] = "南(South)"
-        result[RelativeDirection.LEFT] = "北(North)"
-        result[RelativeDirection.BEHIND] = "西(West)"
-        result[RelativeDirection.AHEAD] = "東(East)"
-    } else if (135 <= rotation && rotation < 225) {
-        result[RelativeDirection.AHEAD] = "南(South)"
-        result[RelativeDirection.BEHIND] = "北(North)"
-        result[RelativeDirection.RIGHT] = "西(West)"
-        result[RelativeDirection.LEFT] = "東(East)"
-    } else if (225 <= rotation && rotation < 315) {
-        result[RelativeDirection.LEFT] = "南(South)"
-        result[RelativeDirection.RIGHT] = "北(North)"
-        result[RelativeDirection.AHEAD] = "西(West)"
-        result[RelativeDirection.BEHIND] = "東(East)"
-    }
-    return result[type] ?: throw IllegalStateException("[CreateGridRegionPlugin]方角処理で重大なエラー。開発者に報告してください。")
-}
 
 private tailrec fun <T> T.repeatedlyApply(time: Int, function: (T) -> T): T = when {
     time < 1 -> this
