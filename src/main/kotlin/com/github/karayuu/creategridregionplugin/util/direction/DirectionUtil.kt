fb02e0c33476e740aecef2ed954e31298d7f1a7b
Kory33 <6561358+kory33@users.noreply.github.com>
2018-01-17 20:16:46 +0800

    CLEAN: DirectionUtilを簡略化する

--- src/main/kotlin/com/github/karayuu/creategridregionplugin/util/direction/DirectionUtil.kt
+++ src/main/kotlin/com/github/karayuu/creategridregionplugin/util/direction/DirectionUtil.kt
@@ -3,37 +3,10 @@ package com.github.karayuu.creategridregionplugin.util.direction
 import org.bukkit.Location
 
 /**
- * Created by karayuu on 2017/12/29
- * Developer of Gigantic☆Seichi Server
- * Support at dev-basic or dev-extreme channel of Discord
- */
-
-private tailrec fun <T> T.repeatedlyApply(time: Int, function: (T) -> T): T = when {
-    time < 1 -> this
-    else -> repeatedlyApply(time - 1, function)
-}
-
-/**
- * [Location]からの相対方向がどの方位を指しているかの対応関係を取得します。
- * @return [RelativeDirection] がどの [CardinalDirection] に対応しているかの [Map]
- */
-fun Location.getRelationToCardinalDirection() : Map<RelativeDirection, CardinalDirection> {
-    val aheadCardinalDirection = CardinalDirection.fromLocation(this)
-
-    // 右向きに回転する回数
-    val turnNumbers = (0 until 4)
-
-    return turnNumbers.map { turnNumber ->
-        val relativeDirection = RelativeDirection.AHEAD.repeatedlyApply(turnNumber) { it.onRightHand() }
-        val cardinalDirection = aheadCardinalDirection.repeatedlyApply(turnNumber) { it.onRightHand() }
-
-        relativeDirection to cardinalDirection
-    }.toMap()
-}
-
-/**
  * [Location]からの相対方向にある方位を取得します。
  * @param direction プレーヤーからの相対方向
  * @return プレーヤーからの指定された方向にある方位を表す[CardinalDirection]
  */
-fun Location.cardinalDirectionOn(direction: RelativeDirection) = getRelationToCardinalDirection()[direction]!!
+fun Location.cardinalDirectionOn(direction: RelativeDirection) = (0 until 4)
+        .first { RelativeDirection.AHEAD.turnRight(it) == direction }
+        .let { turnNumber -> CardinalDirection.fromLocation(this).turnRight(turnNumber) }
