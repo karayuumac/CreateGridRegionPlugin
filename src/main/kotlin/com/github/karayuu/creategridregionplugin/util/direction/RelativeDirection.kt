1d2a597dba29a0fcd382f4c7ce1a2c8e776054bb
Kory33 <6561358+kory33@users.noreply.github.com>
2018-01-17 13:47:20 +0800

    UPDATE: RelativeDirection#turnRightを実装

--- src/main/kotlin/com/github/karayuu/creategridregionplugin/util/direction/RelativeDirection.kt
+++ src/main/kotlin/com/github/karayuu/creategridregionplugin/util/direction/RelativeDirection.kt
@@ -18,4 +18,13 @@ enum class RelativeDirection(val localizedName: String) {
         BEHIND -> LEFT
         LEFT -> AHEAD
     }
+
+    /**
+     * このオブジェクトが指し示す方向から[time]回時計回りに回転した相対方向を求めます。
+     */
+    fun turnRight(time: Int): RelativeDirection = when {
+        time == 0 -> this
+        time < 0 -> turnRight((time % 4) + 4)
+        else -> onRightHand().turnRight((time % 4) - 1)
+    }
 }
