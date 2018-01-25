ee0368ab9e2a56b85afbba4160b13745ae2b3820
Kory33 <6561358+kory33@users.noreply.github.com>
2018-01-26 03:40:21 +0800

    FIX: 初期化順によるNPEを修正する

--- src/main/kotlin/com/github/karayuu/creategridregionplugin/menu/menus/grid/GridMenuSession.kt
+++ src/main/kotlin/com/github/karayuu/creategridregionplugin/menu/menus/grid/GridMenuSession.kt
@@ -1,5 +1,6 @@
 package com.github.karayuu.creategridregionplugin.menu.menus.grid
 
+import com.github.karayuu.creategridregionplugin.menu.InventoryProperty
 import com.github.karayuu.creategridregionplugin.menu.MenuSessionWithSound
 import com.github.karayuu.creategridregionplugin.menu.menus.grid.buttons.*
 import com.github.karayuu.creategridregionplugin.util.SoundConfiguration
@@ -8,35 +9,37 @@ import com.github.karayuu.creategridregionplugin.util.selection.GridSelection
 import org.bukkit.ChatColor
 import org.bukkit.Sound
 import org.bukkit.entity.Player
+import org.bukkit.event.inventory.InventoryType
 import org.bukkit.inventory.InventoryView
 import kotlin.properties.Delegates
 
 class GridMenuSession(private val issueTargetPlayer: Player): MenuSessionWithSound() {
     override val openingSound = SoundConfiguration(Sound.BLOCK_STONE_BUTTON_CLICK_ON, 1F, 1F)
     override val title = "${ChatColor.LIGHT_PURPLE}${ChatColor.BOLD}グリッド式保護メニュー"
+    override val inventoryProperty = InventoryProperty(InventoryType.DISPENSER)
 
-    init {
-        super.buttons = computeButtons()
-    }
-
-    var gridSelection: GridSelection by Delegates.observable(GridSelection()) { _, _, _ ->
-        super.buttons = computeButtons()
-    }
-
-    private val extendOrReduceButtonFactory = { direction: RelativeDirection, iconData: Int ->
-        object: GridExtendOrReduceButton(direction, issueTargetPlayer, gridSelection, this) {
+    private val extendOrReduceButtonFactory = { selection: GridSelection, direction: RelativeDirection, iconData: Int ->
+        object: GridExtendOrReduceButton(direction, issueTargetPlayer, selection, this) {
             override val iconData = iconData
         }
     }
 
-    private fun computeButtons() = mapOf(
-            0 to UnitChangeToggleButton(gridSelection, this),
-            1 to extendOrReduceButtonFactory(RelativeDirection.AHEAD, 14),
-            3 to extendOrReduceButtonFactory(RelativeDirection.LEFT, 10),
-            4 to CurrentGridSelectionButton(gridSelection),
-            5 to extendOrReduceButtonFactory(RelativeDirection.RIGHT, 5),
+    var gridSelection: GridSelection by Delegates.observable(GridSelection()) { _, _, newSelection ->
+        super.buttons = computeButtons(newSelection)
+    }
+
+    init {
+        super.buttons = computeButtons(gridSelection)
+    }
+
+    private fun computeButtons(selection: GridSelection) = mapOf(
+            0 to UnitChangeToggleButton(selection, this),
+            1 to extendOrReduceButtonFactory(selection, RelativeDirection.AHEAD, 14),
+            3 to extendOrReduceButtonFactory(selection, RelativeDirection.LEFT, 10),
+            4 to CurrentGridSelectionButton(selection),
+            5 to extendOrReduceButtonFactory(selection, RelativeDirection.RIGHT, 5),
             6 to ResetButton(this),
-            7 to extendOrReduceButtonFactory(RelativeDirection.BEHIND, 13),
+            7 to extendOrReduceButtonFactory(selection, RelativeDirection.BEHIND, 13),
             8 to CreateRegionButton(issueTargetPlayer)
     )
 
